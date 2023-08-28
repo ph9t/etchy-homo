0bc0e67ca4456e8bf797c89aba02a0e310429675
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2023-08-28 13:50:11 +0800

    feat: add event listeners for changing mode

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const modeList = document.querySelectorAll('ul li')
+
+let currentMode = 'classic'
+
+/* allows for changing current mode */
+modeList.forEach(mode => {
+  mode.addEventListener(
+    'click',
+    e => {
+      currentMode = e.currentTarget.id
+
+      if (currentMode === 'intuitive') {
+        clearGrid()
+      }
+    },
+    { capture: true }
+  )
+})
 function clearGrid() {
   const etchGridSquare = document.querySelectorAll('#etch-grid div')
 
